1923e0a32a9fcd7f7f62338cf77c8e25411f6a39
Thiago Pierre <thiago.barboza.p@gmail.com>
2023-06-14 11:32:40 -0300

    App Structure- CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { Post } from './Post';
+import { Header } from './components/Header';
+import './styles.css'
 
 export function App() {
   return (
     <>
+      <Header />
+      
       <Post 
         author="Diego Fernandes" 
         content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos delectus aperiam accusamus sit ut eius adipisci repellendus. Ratione, obcaecati, cupiditate quos unde qui magni itaque, vero eligendi voluptates nesciunt nobis."
