7f7caa5240f3a0a69efbcf73c34fbc6e6359c00b
Thiago Pierre <thiago.barboza.p@gmail.com>
2023-06-15 09:56:01 -0300

    Done with the UI part

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import { Post } from './Post';
+import { Post } from './components/Post';
 import { Header } from './components/Header';
-import './styles.css'
+import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <>
       <Header />
-      
-      <Post 
-        author="Diego Fernandes" 
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos delectus aperiam accusamus sit ut eius adipisci repellendus. Ratione, obcaecati, cupiditate quos unde qui magni itaque, vero eligendi voluptates nesciunt nobis."
-      />
-      <Post 
-        author="Gabriel Buzzi"
-        content="Um novo post muito legal"
-      />
-      <Post />
+
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar />
+        </aside>
+        <main>
+          <Post 
+          author="Diego Fernandes" 
+          content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos delectus aperiam accusamus sit ut eius adipisci repellendus. Ratione, obcaecati, cupiditate quos unde qui magni itaque, vero eligendi voluptates nesciunt nobis."
+          />
+          <Post 
+            author="Gabriel Buzzi"
+            content="Um novo post muito legal"
+          />
+        </main>
+      </div>
     </>
     )
   }
---
+++ src/components/Comment.jsx
@@ -0,0 +1,32 @@
+import { ThumbsUp, Trash } from '@phosphor-icons/react'
+import styles from './Comment.module.css'
+import { Avatar } from './Avatar'
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/thiagoPierre.png" alt="" />
+            <div className="styles commentBox">
+                <div className="styles commentContent">
+                    <header>
+                        <div className="styles authorAndTime">
+                            <strong>Thiago Pierre</strong>
+                            <time title="11 de Maio às 8:13h" dateTime="2022-05-11 08:13:30" >Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={24}/>
+                        </button>
+                    </header>
+                    <p>Muito bom devon, parabanes!</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
