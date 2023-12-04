743867f911f37a6e9d6ac18889ea60fcf774da04
Thiago Barboza Pierre <thiago.barboza.p@gmail.com>
2023-12-04 08:14:26 -0300

    Inserindo comentários (Programação declarativa)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,47 @@
-import { Post } from './components/Post';
-import { Header } from './components/Header';
-import './global.css'
-import styles from './App.module.css'
-import { Sidebar } from './components/Sidebar';
+import { Post } from "./components/Post";
+import { Header } from "./components/Header";
+import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-05-03 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Educator @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-05-10 20:00:00"),
+  },
+];
 
 export function App() {
   return (
@@ -14,17 +53,17 @@ export function App() {
           <Sidebar />
         </aside>
         <main>
-          <Post 
-          author="Diego Fernandes" 
-          content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos delectus aperiam accusamus sit ut eius adipisci repellendus. Ratione, obcaecati, cupiditate quos unde qui magni itaque, vero eligendi voluptates nesciunt nobis."
-          />
-          <Post 
-            author="Gabriel Buzzi"
-            content="Um novo post muito legal"
-          />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </>
-    )
-  }
-
+  );
+}
